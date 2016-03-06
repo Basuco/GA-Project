b3fcfd5318cd33fe63b69adbddc3b278ef8628db
Basuco <alejandro.g.206@gmail.com>
2016-03-06 16:38:02 -0430

    muestra de resultado en Promgram.cs

--- Samples/Issue1Sample/Issue1Sample/Program.cs
+++ Samples/Issue1Sample/Issue1Sample/Program.cs
@@ -27,7 +27,7 @@ namespace Issue1Sample
             Console.WriteLine("GA done in {0} generations.", ga.GenerationsNumber);
 
             var bestChromosome = ga.BestChromosome as Issue1Chromosome;
-            Console.WriteLine("Best solution found is X:{0}, Y:{1} with {2} fitness.", bestChromosome.X, bestChromosome.Y, bestChromosome.Fitness);
+            Console.WriteLine("Best solution found is A1:{0}, A2:{1}, A3:{2}, A4:{3}, A5:{4}, A6:{5}, A7:{6}, A8:{7}, A9:{8}, A10:{9}, A11:{10}, A12:{11}, A13:{12}, A14:{13}, A15:{14}, A16:{15} with {15} fitness.", bestChromosome.A1, bestChromosome.A2, bestChromosome.A3, bestChromosome.A4, bestChromosome.A5, bestChromosome.A6, bestChromosome.A7, bestChromosome.A8, bestChromosome.A9, bestChromosome.A10, bestChromosome.A11, bestChromosome.A12, bestChromosome.A13, bestChromosome.A14, bestChromosome.A15, bestChromosome.A16, bestChromosome.Fitness);
 		    Console.ReadKey();
         }
     }
